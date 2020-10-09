237ba67355c64e5ae4a8a5e7051c698950522254
Trayan Iliev <office@iproduct.org>
2020-10-09 20:32:33 +0300

    Web service project initial commit

---
+++ webflux-demo/src/test/java/course/dml/webflux/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package course.dml.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
