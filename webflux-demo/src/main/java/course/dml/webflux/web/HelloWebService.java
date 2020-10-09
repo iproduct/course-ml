f6c12b7bd6acd60cf5998308d5c2810ebcb0dd51
Trayan Iliev <office@iproduct.org>
2020-10-09 21:46:51 +0300

    Web service project initial commit

--- webflux-demo/src/main/java/course/dml/webflux/web/HelloWebService.java
+++ webflux-demo/src/main/java/course/dml/webflux/web/HelloWebService.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"/hello/{name}", "/hello"})
 public class HelloWebService {
-    @GetMapping
+    @GetMapping({"/hello/{name}", "/hello"})
     public String hello(@PathVariable(name = "name", required = false) String name) {
         return "Hello from Reactor, " + (name == null ? "Stranger": name) + "!";
     }
