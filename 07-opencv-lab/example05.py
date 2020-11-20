1676be603baf77f2dfbdb1ba840a6ea124c2e89c
Trayan Iliev <office@iproduct.org>
2020-11-20 21:22:20 +0200

    draw bounding contour

--- 07-opencv-lab/example05.py
+++ 07-opencv-lab/example05.py
@@ -1,11 +1,37 @@
 import cv2 as cv
 import sys
 import numpy as np
+from scipy.spatial import distance
+
+corners = []
+
+def is_closing(point):
+    for corner in corners:
+        if distance.euclidean(point, corner) < 2:
+            return True
+    return False
+
+def drawPoint(point):
+    cv.circle(img, point, 3, (0, 0, 255), -1)
+    cv.putText(img, f'({point[0]}, {point[1]})', (point[0] - 100, point[1] + 30), cv.FONT_HERSHEY_SCRIPT_COMPLEX, 0.7, (0, 0, 255), 2, cv.LINE_AA )
+
+def drawBoundary():
+    startPoint = corners[-1]
+    for endPoint in corners:
+        cv.line(img, startPoint, endPoint, (0, 255, 0), 2, cv.LINE_AA)
+        startPoint = endPoint
 
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
     if event == cv.EVENT_LBUTTONDOWN:
-        cv.circle(img,(x,y),3,(0,0,255),-1)
+        point = (x,y)
+        if(is_closing(point)):
+            drawBoundary()
+        else:
+            corners.append(point);
+            drawPoint(point)
+
+
 
 # img = np.zeros((512,512,3), np.uint8)
 
