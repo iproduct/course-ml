d7ef32402fcd7c551c2b98eb9fd3ef52a13de846
Trayan Iliev <office@iproduct.org>
2020-11-20 19:24:12 +0200

    video capture and resizing

--- 07-opencv-lab/example02.py
+++ 07-opencv-lab/example02.py
@@ -1,17 +1,27 @@
 import cv2 as cv
 import sys
 
+WIDTH = 640
+HEIGHT = 480
+
 if __name__ == '__main__':
     print(f'OpenCV imported: {cv.version.opencv_version}')
     # img = cv.imread("resources/lena.png")
     # if img is None:
     #     sys.exit("Could not read the image.")
-    video = cv.VideoCapture("resources/mov_bbb.mp4")
+    # video = cv.VideoCapture("resources/mov_bbb.mp4")
+    video = cv.VideoCapture(0)
+    video.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
+    video.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+
+    cv.namedWindow("Video")
 
     while True:
         success, img = video.read()
         if not success:
             sys.exit("Could not find video.")
+        img = cv.resize(img, (WIDTH, HEIGHT))
+        cv.resizeWindow("Video", WIDTH, HEIGHT)
         cv.imshow("Video", img)
         if cv.waitKey(30) & 0xFF == ord('q'):
             break;
