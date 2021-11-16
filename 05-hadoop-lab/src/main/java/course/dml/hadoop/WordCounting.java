e68d54b731fcc36a601c3be8da425459f165bbe2
Trayan Iliev <office@iproduct.org>
2021-11-16 21:04:17 +0200

    make word counting gradle managed project

---
+++ 05-hadoop-lab/src/main/java/course/dml/hadoop/WordCounting.java
@@ -0,0 +1,9 @@
+package course.dml.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class WordCounting {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+    }
+}
