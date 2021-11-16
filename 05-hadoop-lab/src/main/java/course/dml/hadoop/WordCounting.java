c5d2873053c5dfa8277377f7c8ce44680150ff33
Trayan Iliev <office@iproduct.org>
2021-11-16 21:52:00 +0200

    logback dependencies and config added

--- 05-hadoop-lab/src/main/java/course/dml/hadoop/WordCounting.java
+++ 05-hadoop-lab/src/main/java/course/dml/hadoop/WordCounting.java
@@ -1,9 +1,75 @@
 package course.dml.hadoop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+
+
+@Slf4j
 public class WordCounting {
-    public static void main(String[] args) {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Mapper.Context context
+        ) throws IOException, InterruptedException {
+            log.info("!!!!! Object Key {}", key.toString());
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(WordCounting.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        // Create a new JobConf
+        JobConf jobConf = new JobConf(new Configuration(), WordCounting.class);
+        FileOutputFormat.setOutputPath(jobConf, new Path(args[0]));
+        for (int i = 1; i < args.length; i++) {
+            FileInputFormat.addInputPath(jobConf, new Path(args[i]));
+        }
+        System.exit(job.waitForCompletion(true)? 0 : 1);
     }
 }
