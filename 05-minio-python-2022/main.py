f638fc722a39bb52d743cc159cf6c5397ce0ed82
Trayan Iliev <office@iproduct.org>
2022-11-15 21:52:47 +0200

    minio basic demo added

--- 05-minio-python-2022/main.py
+++ 05-minio-python-2022/main.py
@@ -1,5 +1,23 @@
 from minio import Minio
 from minio.error import InvalidResponseError, MinioException
+import json
+
+
+def list_objects(client, bucket):
+    objects = client.list_objects(bucket.name, recursive=True)
+    for obj in objects:
+        print('-->', obj.bucket_name, obj.object_name, obj.last_modified, obj.etag, obj.size, obj.content_type)
+        # Get data of an object
+        try:
+            response = client.get_object(bucket_name=bucket.name, object_name=obj.object_name)
+            data = json.loads(response.data)
+            json.dumps(data, indent=4)
+            print(json.dumps(data, indent=4))
+        finally:
+            response.close()
+            response.release_conn()
+
+
 
 if __name__ == '__main__':
     # Initialize Minio client
@@ -14,6 +32,14 @@ if __name__ == '__main__':
         raise
 
     # Add object to bucket
+    try:
+        client.fput_object('posts', 'posts.json', './posts.json', content_type='application/json')
+    except MinioException as err:
+        print('Minio exception:', err)
+    except InvalidResponseError as err:
+        print('Invalid response error:', err)
+
+    # Copy object under new name
     try:
         client.fput_object('posts', 'posts.json', './posts.json', content_type='application/json')
     except MinioException as err:
