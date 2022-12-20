a761314c2306c30e799c2c39af13e2871d6d3efa
Trayan Iliev <office@iproduct.org>
2022-12-20 21:42:15 +0200

    dnn face detection model added

--- fmi-2022-09-image-recognition-opencv-dnn/cv_face_detection_haarcascade_02.py
+++ fmi-2022-09-image-recognition-opencv-dnn/cv_face_detection_haarcascade_02.py
@@ -18,6 +18,27 @@ if __name__ == '__main__':
          sys.exit('Could not capture a frame')
       gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+      faces = face_cascade.detectMultiScale(
+         gray,
+         scaleFactor=1.1,
+         minNeighbors=5,
+         minSize=(30, 30)
+      )
+
+      for (x, y, w, h) in faces:
+         cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+         roi_gray = gray[y:y+h, x:x+w]
+         roi_color = img[y:y+h, x:x+w]
+         eyes = eye_cascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.1,
+            minNeighbors=3,
+            minSize=(8, 5),
+            maxSize=(60, 40)
+         )
+         for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
+
       # fps = video.get(cv.CAP_PROP_FPS)
       # print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
 
@@ -27,7 +48,7 @@ if __name__ == '__main__':
       cv.putText(img, 'FPR : {0}'.format(fps), (100, 30), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
       cv.imshow('My Video', img)
-      cv.imshow('Gray Video', gray)
+      # cv.imshow('Gray Video', gray)
       start = time.time()
 
       if cv.waitKey(20) & 0xFF == ord('q'):
