0d149731733008f5ef7aca451925b27e42bcd84e
Trayan Iliev <office@iproduct.org>
2023-10-11 20:33:11 +0300

    obstacle avoidance demo

--- fmi_2023_01_lego_robot/main.py
+++ fmi_2023_01_lego_robot/main.py
@@ -35,13 +35,26 @@ infrared_sensor = InfraredSensor(Port.S3)
 # touch the ground.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=32, axle_track=172)
 
-# Write your program here.
+# Play a sound to tell us when we are ready to start moving
 ev3.speaker.beep()
 ev3.screen.draw_text(10,10, 'Hi')
 ev3.speaker.set_speech_options('en', 'm1', 150, 50)
 ev3.speaker.set_volume(100)
-ev3.speaker.say('''I like to talk because I am a robot.''')
+ev3.speaker.say('''I like to talk because I am a robot.
+Did you know that robots like to make sounds?
+Beep. Boop. Dit. Dit. Meep.
+I am just such a chatterbox.''')
 
+# Run the grip motor up to 500 degrees per second. To a target angle of 360 degrees.
+# grip_motor.run_target(500, 360)
+
+# robot.straight(300)
+# robot.turn(90)
+
+# The following loop makes the robot drive forward until it detects an
+# obstacle. Then it backs up and turns around. It keeps on doing this
+# until you stop the program.
+i = 0
 while True:
     # Begin driving forward at 200 millimeters per second.
     robot.drive(100, 0)
@@ -49,7 +62,6 @@ while True:
     # Wait until an obstacle is detected. This is done by repeatedly
     # doing nothing (waiting for 10 milliseconds) while the measured
     # distance is still greater than 300 mm.
-    i = 0
     while not touch_sensor.pressed():
         wait(10)
         if i%10 == 0:
@@ -66,3 +78,12 @@ while True:
 
     # Turn around by 90 degrees
     robot.turn(90)
+
+
+
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+# grip_motor.run_target(500, 360)
+# grip_motor.run_until_stalled(-500, then=Stop.HOLD, duty_limit=150)
+
+# Play another beep sound.
+ev3.speaker.beep(1000, 200)
