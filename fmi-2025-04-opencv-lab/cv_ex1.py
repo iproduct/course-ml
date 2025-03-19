f4210fe4b54bd3fd207843f554d37d7f1f67b976
Trayan Iliev <office@iproduct.org>
2025-03-19 20:33:06 +0200

    opencv lab - ex1 added

--- fmi-2025-04-opencv-lab/cv_ex1.py
+++ fmi-2025-04-opencv-lab/cv_ex1.py
@@ -1,4 +1,10 @@
 import cv2
 
 if __name__ == "__main__":
-    pass
+    img1 = cv2.imread('lena.bmp')
+    print(img1.shape)
+    print(img1.dtype)
+    print(type(img1))
+    cv2.imshow('Lena', img1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
