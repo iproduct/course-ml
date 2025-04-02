315b1ec20966b0521f1722d23b1f315413e9f845
Trayan Iliev <office@iproduct.org>
2025-04-02 16:54:38 +0300

    demos small version updates

--- fmi-2025-03-ann-cnn/keras_custom_gradient_optimization.py
+++ fmi-2025-03-ann-cnn/keras_custom_gradient_optimization.py
@@ -3,14 +3,14 @@ import datetime
 import tensorflow as tf
 from keras import layers
 from keras import models
-from keras.datasets import mnist
-from keras.utils import to_categorical
+from keras.api.datasets import mnist
+from keras.api.utils import to_categorical
 import os
 
 if __name__ == '__main__':
     os.environ["XLA_FLAGS"] = '--xla_gpu_cuda_data_dir="D:/Program Files/CUDA/v11.2/development"'
-    physical_devices = tf.config.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(physical_devices[0], True) # important!
+    # physical_devices = tf.config.list_physical_devices('GPU')
+    # tf.config.experimental.set_memory_growth(physical_devices[0], True) # important!
     tf.config.optimizer.set_jit(True)
 
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     optimizer = tf.keras.optimizers.Adam()
 
     # Prepare a loss function.
-    loss_fn = tf.keras.losses.kl_divergence
+    loss_fn = tf.keras.losses.KLD
     # loss_object = tf.keras.losses.MeanSquaredError()
 
 
